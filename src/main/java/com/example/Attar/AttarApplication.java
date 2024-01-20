c8d4d1fdddcb2fa0c3f0819b34eb551c03cc5373
Mohsin772 <156443531+Mohsin772@users.noreply.github.com>
2024-01-20 09:55:15 +0530

    second

---
+++ src/main/java/com/example/Attar/AttarApplication.java
@@ -0,0 +1,13 @@
+package com.example.Attar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttarApplication.class, args);
+	}
+
+}
